ea6a8217ce5a5aa4e18a26abc608dbb98464c74a
Michal Lurie <Michal.Lurie@gmail.com>
2025-05-29 21:53:56 +0200

    Adjusted scale_availableWidth

--- include/L2/incplot/config.hpp
+++ include/L2/incplot/config.hpp
@@ -72,7 +72,7 @@ public:
     static constexpr size_t min_plotWidth = 24uz;
     static constexpr size_t max_plotWidth = 256uz;
     static constexpr size_t default_targetWidth = 64uz;
-    static constexpr double scale_availablePlotWidth = 0.9;
+    static constexpr double scale_availablePlotWidth = 0.75;
 
     static constexpr size_t min_plotHeight = 5uz;
 
