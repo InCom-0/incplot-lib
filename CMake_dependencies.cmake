c8855192bc62db0f9bcc7f830dc7c4501cedc0b3
Michal Lurie <Michal.Lurie@gmail.com>
2025-10-24 13:10:19 +0200

    Fix: also cleaved argparse from CMake_dependecies

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -36,13 +36,6 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(utf-cpp)
 
 FetchContent_Declare(
-    argparse
-    GIT_REPOSITORY https://github.com/p-ranav/argparse.git
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(argparse)
-
-FetchContent_Declare(
     magic_enum
     GIT_REPOSITORY https://github.com/Neargye/magic_enum.git
     GIT_TAG v0.9.7
